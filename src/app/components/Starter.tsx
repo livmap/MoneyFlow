cedde2f7f7fd626d9924d3b079b79326b537d78b
Prince Maphupha <princemaphupha@Princes-Air.home>
2025-01-19 12:32:33 +0200

    Feature: Starter Page

---
+++ src/app/components/Starter.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Starter = () => {
+  return (
+    <div className='w-screen h-screen bg-white absolute top-0 left-0 flex items-center justify-center'>
+        <Image src={'/icon.png'} alt={'The Logo Icon'} width={300} height={300} />
+    </div>
+  )
+}
+
+export default Starter
