c77ce3147dd2798c53ffaf85d7d8181e50290fcc
Prince Maphupha <princemaphupha@Princes-Air.home>
2025-01-19 12:44:30 +0200

    Changed web icon

--- src/app/components/Starter.tsx
+++ src/app/components/Starter.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Starter = () => {
   return (
     <div className='w-screen h-screen bg-white absolute top-0 left-0 flex items-center justify-center'>
-        <Image src={'/icon.png'} alt={'The Logo Icon'} width={300} height={300} />
+        <Image src={'/icon.png'} alt={'The Logo Icon'} width={300} height={300} priority />
     </div>
   )
 }
